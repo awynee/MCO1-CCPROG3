ef924111df42a1cae05c8d1e1f55fe289160c38e
awynee <164032813+awynee@users.noreply.github.com>
2025-10-07 18:56:19 +0800

    added reservation class and guest class modified propertylisting

---
+++ MCO1/src/Guest.java
@@ -0,0 +1,11 @@
+public class Guest {
+    private String guestName;
+
+    public Guest(String guestName) {
+        this.guestName = guestName;
+    }
+
+    public String getGuestName() {
+        return guestName;
+    }
+}
--- MCO1/src/Reservation.java
+++ MCO1/src/Reservation.java
@@ -1,2 +1,29 @@
 public class Reservation {
+    private int day;
+    private double totalPrice;
+    private Guest guest;
+
+    public Reservation(int day, double totalPrice, Guest guest) {
+        this.day = day;
+        this.totalPrice = totalPrice;
+        this.guest = guest;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public Guest getGuest() {
+        return guest;
+    }
+
+    public void displayReservationInfo() {
+        System.out.println("Reservation day: " + day);
+        System.out.println("Total price: " + totalPrice);
+        System.out.println("Guest: " + guest.getGuestName());
+    }
 }
